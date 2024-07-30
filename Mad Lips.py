20c0eb302de631074102981a20a7ca9043f7ca2d
BodiIrvine <bodiirvine@gmail.com>
2024-07-30 13:44:55 -0700

    Mad Lips
    I learned a lot doing this project. but now that i can see it after testing it it seems easier to understand and it looks more complicated than it is.

---
+++ Mad Lips.py
@@ -0,0 +1,22 @@
+
+# I decided to use your example for my mad lips
+noun_1 = input('noun: ')
+
+noun_2 = input('plural noun: ')
+
+noun_3 = input('noun: ')
+
+place = input('place: ')
+
+adjective = input('adjective: ')
+
+# it took me a few tries to get it to work but i am very proud of my work
+
+print("Did you know I have a pet " + noun_1 + "? "
+      "He likes to run around and play with all of the " + noun_2 + ". "
+      "One morning, I woke up and he was wearing a " + noun_3 + " for a hat! "
+      "I especially like to take him to the " + place + " "
+      "because he shows off his " + adjective + " side.")
+
+
+
