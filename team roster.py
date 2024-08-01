9e60c4b1c15c7e51a25dac8aab40566a4a86ad08
BodiIrvine <bodiirvine@gmail.com>
2024-08-01 12:31:01 -0700

    team roster
    this was a quick refresher with input and was more fun than I expected for a 15 minute project.

---
+++ team roster.py
@@ -0,0 +1,4 @@
+name = input("please tell us your name")
+team_number = input("please, enter your team number")
+
+print("Welcome to the team, " + name + " Your team number is " + name +", number " + str(team_number))
