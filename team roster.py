30c758ace7f013c099914fd5553bb73f5221ba32
BodiIrvine <bodiirvine@gmail.com>
2024-08-01 12:50:19 -0700

    Update team roster.py

--- team roster.py
+++ team roster.py
@@ -1,4 +1,6 @@
+#here is the input 
 name = input("please tell us your name")
 team_number = input("please, enter your team number")
 
+#here is where the printing takes place
 print("Welcome to the team, " + name + " Your team number is " + name +", number " + str(team_number))
