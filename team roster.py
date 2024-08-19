9f27c4381c8782eb1707d835b5072ca4cb894c74
BodiIrvine <bodiirvine@gmail.com>
2024-08-19 12:29:01 -0700

    Delete team roster.py

--- team roster.py
+++
@@ -1,6 +0,0 @@
-#here is the input 
-name = input("please tell us your name")
-team_number = input("please, enter your team number")
-
-#here is where the printing takes place
-print("Welcome to the team " + name + ", number " + str(team_number))
